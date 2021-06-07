f28631c76c0948c8bf4381e8d38a07546f4a8bfe
Etienne Zink <etienne-zink@gmx.de>
2021-06-07 15:57:01 +0200

    Update von der Compiler.hs. Kann nun das ganze Verzeichnis in der sie liegt parsen und in der Typisierung berücksichtigen.

--- Compiler.hs
+++ Compiler.hs
@@ -1,14 +1,34 @@
 module Compiler where
 
 import Parser
-import TypeCheck
+import TypeCheck ( typeCheckClass )
 import Jvm.Data.ClassFormat
 import TypedAST
+import System.Directory
+import Data.List
+import qualified AbstrakteSyntax as AS
 
-compiler:: String -> Typed Class
-compiler =  extract . typeCheckClass [] [] . parser 
+parseToTypedSyntax:: String -> [AS.Class] -> Typed Class
+parseToTypedSyntax s classes =  extract . typeCheckClass [] classes $ parser s
+    
+parseFiles:: [String] -> [AS.Class]
+parseFiles [] = []
+parseFiles s = map parser s
+
+readFiles :: [FilePath] -> [IO String]
+readFiles = map readFile
 
 extract (Right r) = r
 extract (Left e) = error e 
 
+
+main = do
+    s <- getContents
+    fileNames <- getDirectoryContents "."
+    let filtered = filter (isSuffixOf "java") fileNames
+    fileContents <- traverse readFile filtered
+    let classes = parseFiles fileContents
+    
+    let abstractTypedClass =  parseToTypedSyntax s classes
+    print abstractTypedClass
 
